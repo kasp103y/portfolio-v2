08673b7bc98c463eab34d7ae49c107382c2542ac
Kasper Nikolajsen Dahl <kasp103y@stud.kea.dk>
2022-03-29 17:38:14 +0200

    hover-portfolio

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const ub = document.querySelector("#ub");
 const grob = document.querySelector("#grob");
 const spil = document.querySelector("#spil");
 const body = document.querySelector("body");
-const projekt = document.querySelector("projekt");
 
 bogino.addEventListener("mouseenter", boginoFarve);
 bogino.addEventListener("mouseleave", boginoNot);
@@ -27,44 +26,55 @@ function boginoFarve() {
 
 function boginoNot() {
   body.classList.remove("red");
+  projekt.innerHTML = "";
 }
 
 function musikFarve() {
   body.classList.add("purple");
+  projekt.innerHTML = "MusikLoftet";
 }
 
 function musikNot() {
   body.classList.remove("purple");
+  projekt.innerHTML = "";
 }
 
 function cosmicFarve() {
   body.classList.add("darkpurple");
+  projekt.innerHTML = "Cosmic font";
 }
 
 function cosmicNot() {
   body.classList.remove("darkpurple");
+  projekt.innerHTML = "";
 }
 
 function ubFarve() {
   body.classList.add("greenub");
+  projekt.innerHTML = "Ungdomsbyen";
 }
 
 function ubNot() {
   body.classList.remove("greenub");
+  projekt.innerHTML = "";
 }
 
 function grobFarve() {
   body.classList.add("greengrob");
+  projekt.innerHTML = "Teater Grob";
 }
 
 function grobNot() {
   body.classList.remove("greengrob");
+  projekt.innerHTML = "";
 }
 
 function spilFarve() {
   body.classList.add("greenspil");
+  projekt.innerHTML = "Save Thy Flower";
 }
 
 function spilNot() {
   body.classList.remove("greenspil");
+  projekt.innerHTML = "";
 }
